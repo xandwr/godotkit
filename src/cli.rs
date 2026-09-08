5b23e592db785391b692e220903bd212dab16590
Xander Pickering <94504311+xandwr@users.noreply.github.com>
2026-09-07 18:16:58 -0600

    Formatter tooltip

--- src/cli.rs
+++ src/cli.rs
@@ -11,7 +11,7 @@ pub struct Cli {
 
 #[derive(Debug, Subcommand)]
 pub enum Command {
-    #[command(about = "Compact bare-return guards in GDScript 4.7.2")]
+    #[command(about = "Format GDScript source files")]
     Format(FormatArgs),
 }
 
